2fec4fa114818a2f3def3fbd677592126653e06a
frankdunn <frankdunn37@gmail.com>
2018-07-21 20:40:38 +0200

    Update plco1.ino

--- plco1.ino
+++ plco1.ino
@@ -3,17 +3,20 @@
 
 Plc01 myplc ;
 uint8_t counter = 0;
-
+const int interval = 20;
+unsigned long previousMillis = 0; 
 void setup() {
   myplc.initRelay();
 }
 
 void loop() { // put your main code here, to run repeatedly:
-  
-  myplc.CLT01_38S_GetInpData();
-  myplc.VNI8200XP_SendOutputData(counter);
-  counter++;
-  delay(40);
+  if(millis() - previousMillis > interval) {
+    previousMillis = millis();
+    myplc.CLT01_38S_GetInpData();
+    myplc.VNI8200XP_SendOutputData(counter);
+    counter++;
+  }
 }
+
 
 
